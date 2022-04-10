1c01a4099e15fd9335d95f8c934015ce64839c4b
dohyun <ehgus4688@gmail.com>
2022-04-10 21:59:45 +0900

    4/10 커밋

---
+++ app/src/main/java/com/kdh/diarydodo/data/query/DiaryQuery.kt
@@ -0,0 +1,11 @@
+package com.kdh.diarydodo.data.query
+
+import com.kdh.diarydodo.data.DiaryInfo
+
+class DiaryQuery {
+
+
+    suspend fun insertDiary(memo : DiaryInfo){
+
+    }
+}
--- app/src/main/java/com/kdh/diarydodo/ui/home/WriteDiaryFragment.kt
+++ app/src/main/java/com/kdh/diarydodo/ui/home/WriteDiaryFragment.kt
@@ -7,12 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.kdh.diarydodo.R
 import com.kdh.diarydodo.data.db.DiaryDataBase
+import com.kdh.diarydodo.databinding.FragmentReadDiaryBinding
+import com.kdh.diarydodo.databinding.FragmentWriteDiaryBinding
+import kotlinx.android.synthetic.main.fragment_write_diary.view.*
 
 
 class WriteDiaryFragment : Fragment() {
 
     lateinit var db : DiaryDataBase
-
+    private var mBinding: FragmentWriteDiaryBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,13 +24,26 @@ class WriteDiaryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_write_diary, container, false)
+        db = context?.let { DiaryDataBase.getInstance(it) }!!
+     //   return inflater.inflate(R.layout.fragment_write_diary, container, false)
+
+        val binding = FragmentWriteDiaryBinding.inflate(inflater, container, false)
+        mBinding = binding
+        return mBinding?.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentWriteDiaryBinding.inflate(layoutInflater)
+
+        binding.diaryReg.setOnClickListener{
+            
+        }
 
 
     }
+
+
 
 }
